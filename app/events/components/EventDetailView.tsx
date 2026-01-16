62ea94acbac25810841b0afcef68fc022673c706
= <=>
2026-01-16 15:15:22 +0100

    feat: event module

---
+++ app/events/components/EventDetailView.tsx
@@ -0,0 +1,39 @@
+import styles from "../Events.module.css";
+import { type AnnouncementDTO } from "../eventsTypes";
+
+interface Props {
+  announcement: AnnouncementDTO | null;
+}
+
+export default function EventMainView({ announcement }: Props) {
+  if (!announcement) {
+    return (
+      <div className={styles.mainView}>
+        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%', color: '#aaa'}}>
+          Wybierz wydarzenie, aby zobaczyć szczegóły.
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.mainView}>
+      <div className={styles.detailImage}>
+        Nagłówek / Zdjęcie
+      </div>
+      
+      <h1 className={styles.detailHeader}>{announcement.title}</h1>
+      
+      <p className={styles.detailContent}>
+        {announcement.content}
+      </p>
+
+      <div className={styles.detailFooter}>
+        <span>Autor ID: {announcement.authorId}</span>
+        <span>
+            Odbiorcy: {announcement.groupId ? `Grupa #${announcement.groupId}` : "Wszyscy"}
+        </span>
+      </div>
+    </div>
+  );
+}
